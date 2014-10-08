ddf7d515a4ccd16b9a521797c8f49e19b008b786
Scott Pullen <s.pullen05@gmail.com>
2014-10-07 20:40:33 -0400

    adding css/js assets

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -3,8 +3,15 @@
 <head>
   <meta charset="UTF-8">
   <title>Developer's Best Friend</title>
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
 </head>
 <body>
-  @yield('body')
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">@yield('body')</div>
+  </div>
+</div>
+<script src="{{ asset('js/jquery-2.1.1.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </body>
 </html>
--- app/views/lorem_ipsum.blade.php
+++ app/views/lorem_ipsum.blade.php
@@ -3,9 +3,9 @@
 @section('body')
   {{ Form::open(array('route' => 'generate_lorem_ipsum', 'class' => 'form-inline')) }}
     <div class="form-group">
-      {{ Form::label('number_of_paragraphs', 'Number of paragraphs') }}
-      {{ Form::number('number_of_paragraphs', isset($numberOfParagraphs) ? $numberOfParagraphs : 5) }}
-      {{ Form::submit('Generate') }}
+      {{ Form::label('number_of_paragraphs', 'Number of paragraphs', array('class' => 'control-label')) }}
+      {{ Form::number('number_of_paragraphs', isset($numberOfParagraphs) ? $numberOfParagraphs : 5, array('class' => 'form-control')) }}
+      {{ Form::submit('Generate', array('class' => 'btn btn-primary')) }}
     </div>
   {{ Form::close() }}
 
