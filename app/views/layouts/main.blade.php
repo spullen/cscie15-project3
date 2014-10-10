4c6b746d4afb9ae6bc13b353520799668ed57b15
Scott Pullen <s.pullen05@gmail.com>
2014-10-09 21:27:13 -0400

    adding options for user generator

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
   <title>Developer's Best Friend</title>
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
 </head>
--- app/views/user_generator.blade.php
+++ app/views/user_generator.blade.php
@@ -1,5 +1,67 @@
 @extends('layouts.main')
 
 @section('body')
-  TODO: user-generator
+  {{ Form::open(array('action' => 'UserGeneratorController@postCreate', 'class' => 'form-horizontal')) }}
+    <div class="form-group">
+      {{ Form::label('number_of_users', 'Number of users', array('class' => 'col-md-2 col-sm-2 control-label')) }}
+      <div class="col-md-2 col-sm-2">
+        {{ Form::number('number_of_users', isset($numberOfUsers) ? $numberOfUsers : 5, array('class' => 'form-control')) }}
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-md-offset-2 col-md-6 col-sm-offset-2 col-sm-6">
+        <div class="checkbox">
+          <label>
+            {{ Form::checkbox('include_email', '1', (isset($includeEmail) && $includeEmail)) }} Include email?
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-md-offset-2 col-md-6 col-sm-offset-2 col-sm-6">
+        <div class="checkbox">
+          <label>
+            {{ Form::checkbox('include_uuid', '1', (isset($includeUUID) && $includeUUID)) }} Include UUID?
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-md-offset-2 col-md-6 col-sm-offset-2 col-sm-6">
+        <div class="checkbox">
+          <label>
+            {{ Form::checkbox('include_birthdate', '1', (isset($includeBirthdate) && $includeBirthdate)) }} Include birthdate?
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-md-offset-2 col-md-6 col-sm-offset-2 col-sm-6">
+        <div class="checkbox">
+          <label>
+            {{ Form::checkbox('include_location', '1', (isset($includeLocation) && $includeLocation)) }} Include location?
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      {{ Form::label('locale', 'Locale', array('class' => 'col-md-2 col-sm-2 control-label')) }}
+      <div class="col-md-2 col-sm-2">
+        {{ Form::select('locale', array(
+              'en_US' => 'United States',
+              'fr_FR' => 'France',
+              'de_DE' => 'Germany',
+              'jp_JP' => 'Japan',
+              'ru_RU' => 'Russia',
+              'es_ES' => 'Spain',
+              'it_IT' => 'Italy'
+        ), isset($locale) ? $locale : 'en_US', array('class' => 'form-control')) }}
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-md-offset-2 col-md-6 col-sm-offset-2 col-sm-6">
+        {{ Form::submit('Generate', array('class' => 'btn btn-primary')) }}
+      </div>
+    </div>
+  {{ Form::close() }}
 @stop
