d3ab3583622d67698176a593e4b00e752c9478ba
Scott Pullen <s.pullen05@gmail.com>
2014-10-09 22:08:25 -0400

    justing moving pg into postCreate instead of getCreate

--- app/controllers/UserGeneratorController.php
+++ app/controllers/UserGeneratorController.php
@@ -3,12 +3,11 @@
 class UserGeneratorController extends BaseController {
 
   public function getCreate() {
-    $pg = new PasswordGenerator(array('number_of_words' => 4));
-    
     return View::make('user_generator');
   }
 
   public function postCreate() {
+    $pg = new PasswordGenerator(array('number_of_words' => 4));
     return View::make('user_generator');
   }
 }
