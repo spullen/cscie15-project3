d801ca4be9f46923932ad3dbe46dfe57a80a6d0c
Scott Pullen <s.pullen05@gmail.com>
2014-10-12 21:02:47 -0400

    adding validation to form

--- app/controllers/LoremIpsumController.php
+++ app/controllers/LoremIpsumController.php
@@ -7,12 +7,23 @@ class LoremIpsumController extends BaseController {
   }
 
   public function postCreate() {
-    $numberOfParagraphs = Input::get('number_of_paragraphs');
+    $paragraphs = null;
 
-    $generator = new Badcow\LoremIpsum\Generator();
-    $paragraphs = $generator->getParagraphs($numberOfParagraphs);
+    $data = Input::all();
 
+    $rules = array(
+      'number_of_paragraphs' => array('required', 'numeric', 'between:1,100')
+    );
+
+    $validator = Validator::make($data, $rules);
+
+    if($validator->passes()) {
+      $generator = new Badcow\LoremIpsum\Generator();
+      $paragraphs = $generator->getParagraphs($data['number_of_paragraphs']);
+    }
+    
     return View::make('lorem_ipsum')
-      ->with('paragraphs', $paragraphs);
+      ->with('paragraphs', $paragraphs)
+      ->withErrors($validator);
   }
 }
--- app/views/lorem_ipsum.blade.php
+++ app/views/lorem_ipsum.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.main')
 
 @section('body')
-  {{ Form::open(array('action' => 'LoremIpsumController@postCreate', 'class' => 'form-inline')) }}
+  {{ Form::open(array('action' => 'LoremIpsumController@postCreate', 'class' => 'form-horizontal')) }}
+    <div class="form-group {{ $errors->has('number_of_paragraphs') ? 'has-error' : '' }}">
+      {{ Form::label('number_of_paragraphs', 'Number of paragraphs', array('class' => 'col-md-2 col-sm-2 control-label')) }}
+      <div class="col-md-4 col-sm-4">
+        {{ Form::number('number_of_paragraphs', Input::get('number_of_paragraphs', 5), array('class' => 'form-control', 'min' => 1, 'max' => 100)) }}
+        <span class="help-block">Number between 1 and 100</span>
+        @if ($errors->has('number_of_paragraphs'))
+          <span class="help-block">{{ $errors->first('number_of_paragraphs') }}</span>
+        @endif
+      </div>
+    </div>
     <div class="form-group">
-      {{ Form::label('number_of_paragraphs', 'Number of paragraphs', array('class' => 'control-label')) }}
-      {{ Form::number('number_of_paragraphs', Input::get('number_of_paragraphs', 5), array('class' => 'form-control')) }}
-      {{ Form::submit('Generate', array('class' => 'btn btn-primary')) }}
+      <div class="col-md-offset-2 col-md-6 col-sm-offset-2 col-sm-6">
+        {{ Form::submit('Generate', array('class' => 'btn btn-primary')) }}
+      </div>
     </div>
   {{ Form::close() }}
 
