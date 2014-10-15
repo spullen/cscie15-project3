58445721dd8382d35803e6612106e1603dcdd07d
Scott Pullen <s.pullen05@gmail.com>
2014-10-14 22:41:36 -0400

    adding a description to the home page about the site

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,7 +3,9 @@
 @section('body')
 <div class="row"><div class="col-md-12 col-sm-12 col-xs-12">
   <p>
-    TODO: about this site
+    Developer's best friend is an application to generate data that can be used on your own application.
+    You can generate Lorem Ipsum text, which can be used to fill in a website. The text is unreadable, which allows the client (or yourself) to focus on the design more than what the text says.
+    You can also generate fake user information, which can be used to test your application.
   </p>
   <p>
     <div><a href="{{ action('LoremIpsumController@getCreate') }}">Lorem Ipsum Generator</a></div>
